af1bd800bb50cef40b2dcf20dfc797758200297b
Ash Martin <rested40@gmail.com>
2026-03-23 08:36:34 -0600

    added astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: netlify(),
   integrations: [mdx(), react(), keystatic()],
 });
